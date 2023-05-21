d65eb41d2d39f6611139165ed4bf8932930119e4
Nicolas Santini <santini.nz@gmail.com>
2023-05-22 11:18:03 +1200

    Update main.tf
    Potential bug setting the user name for the record

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "cloudflare_record" "dids" {
   for_each = { for key in local.dids : key.username => key }
 
   zone_id = var.cloudflare_zone_id
-  name    = each.key
+  name    = each.value.username
   value   = each.value.did
   type    = "TXT"
   ttl     = 300
